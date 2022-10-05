d50524edd81535fa08e42457aa206cd0791cae59
KilianZimmerer <21954712+KilianZimmerer@users.noreply.github.com>
2022-10-05 10:52:04 +0200

    fix webpack

--- next.config.js
+++ next.config.js
@@ -1,13 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+const withPlugins = require('next-compose-plugins')
+const withImages = require('next-images')
+const path = require('path')
 
-module.exports = {
+module.exports = withPlugins([[withImages]], {
   webpack: (
     config,
     { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-  ) => {
+    ) => {
+    config.resolve.modules.push(path.resolve('./'))
+    if (!isServer) {
+      config.node = {
+        fs: 'empty'
+      }
+    }
     config.module.rules.push({
       test: /\.html$/i,
       use: [
@@ -17,6 +22,12 @@ module.exports = {
       ],
     });
 
-    return config;
+    return config
   },
-};
+  target: "serverless",
+  i18n: {
+    locales: ['de', 'en'],
+    defaultLocale: 'de'
+  },
+  distDir: 'build',
+})
